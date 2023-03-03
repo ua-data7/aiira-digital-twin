ae5988bd7d23d76c5ca661622179373b7295c68c
Michelle Yung <myung@email.arizona.edu>
2023-03-03 16:51:40 -0700

    add local settings file

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i%its(11b1(!0a8f91--(6=s31%btt6y9w3huqf*%kmcdr=g6s'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
---
+++ backend/settings/local_example.py
@@ -0,0 +1,12 @@
+from .base import *
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = ""
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
