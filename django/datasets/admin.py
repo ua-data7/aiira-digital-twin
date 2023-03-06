b95769d943bdaae9a3fb5613ad26845d352e8b02
Michelle Yung <myung@email.arizona.edu>
2023-03-06 14:14:33 -0700

    rework project structure

---
+++ django/datasets/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from datasets.models import Dataset
+
+class DatasetAdmin(admin.ModelAdmin):
+
+    list_display = ('display_name', 'data_store_path')
+
+admin.site.register(Dataset, DatasetAdmin)
---
+++ django/datasets/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Dataset(models.Model):
+
+  class DatasetPermissions(models.TextChoices):
+        PUBLIC = 'public', _('Public')
+        PRIVATE = 'private', _('Private')
+
+  display_name = models.CharField(max_length=100)
+  description = models.TextField()
+
+  data_store_path = models.CharField(max_length=255)
+  permissions = models.CharField(max_length=7, choices=DatasetPermissions.choices)
