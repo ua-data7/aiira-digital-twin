43db7059ecb1529178454ac2e569d2da0146d63a
Michelle Yung <myung@arizona.edu>
2023-07-25 17:17:13 +0000

    prettier

--- frontend/src/components/datasets/DatasetDirectory.tsx
+++ frontend/src/components/datasets/DatasetDirectory.tsx
@@ -1,5 +1,9 @@
 import { Stack, Text, Icon, IconButton } from "@chakra-ui/react";
 import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
   Table,
   Thead,
   Tbody,
@@ -9,18 +13,10 @@ import {
   TableContainer,
 } from "@chakra-ui/react";
 
-import {
-  Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-} from "@chakra-ui/react";
-
 import { FcFolder, FcFile } from "react-icons/fc";
 import { DownloadIcon } from "@chakra-ui/icons";
 
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 import type { Dataset, DirectoryArray } from "./DatasetTypes";
 
@@ -37,8 +33,12 @@ type BreadcrumbMenuProps = {
   datasetId: number;
 };
 
-function BreadcrumbMenu({ currentPath, rootPath, nextPath, datasetId }: BreadcrumbMenuProps) {
-  
+function BreadcrumbMenu({
+  currentPath,
+  rootPath,
+  nextPath,
+  datasetId,
+}: BreadcrumbMenuProps) {
   // root path of dataset (top-level folder)
   const rootPathSplit = rootPath.split("/");
 
@@ -46,47 +46,42 @@ function BreadcrumbMenu({ currentPath, rootPath, nextPath, datasetId }: Breadcru
   const currentPathSplit = currentPath.split("/");
 
   // full Next URL path
-  const nextPathSplit = nextPath.split("/")
+  const nextPathSplit = nextPath.split("/");
 
-  console.log(rootPathSplit)
-  console.log(currentPathSplit)
-  console.log(nextPathSplit)
-  
-  // get starting index of dataset root path within full iplant path.
-  // we only want to show Breadcrumb menu starting at dataset root folder, not include 'iplant/home/...)
+  // get items for Breadcrumb menu, starting at dataset root folder
   const rootStartIndex = rootPathSplit.length - 1;
   const menuItems = currentPathSplit.slice(rootStartIndex);
 
-  const datasetPath = nextPathSplit.slice(0, nextPathSplit.length - menuItems.length);
-  
+  // get URL path up until dataset root path
+  const pathPrefix = nextPathSplit.slice(
+    0,
+    nextPathSplit.length - menuItems.length
+  );
 
-  console.log(nextPath)
-
-  function getHref(index:number) {
-
-    const pathItems = menuItems.slice(0, index + 1)
-
-    console.log(datasetPath)
-    console.log(pathItems)
-    
-    const fullPath = datasetPath.join("/") + "/" + pathItems.join("/")
-    
-    return fullPath
-
-    // http://149.165.153.95:3005/datasets/1/iplant/home/shared/commons_repo/curated/mosaic_raamp2/Codebase
-
-    // http://149.165.153.95:3005/datasets/1/iplant/home/shared/mosaic_raamp2/Codebase
-    
+  // build link for each breadcrumb item
+  function getHref(index: number) {
+    const pathItems = menuItems.slice(0, index + 1);
+    const fullPath = pathPrefix.join("/") + "/" + pathItems.join("/");
+    return fullPath;
   }
 
-  // TODO: add href links for breadcrumbs
-
   return (
-    <Breadcrumb fontWeight='medium' fontSize='sm'>
+    <Breadcrumb fontWeight="medium" fontSize="sm">
       {menuItems.map((item, index) => {
         return (
-          <BreadcrumbItem key={index} isCurrentPage={index === menuItems.length - 1}>
-            <BreadcrumbLink href={getHref(index)} fontWeight={index === menuItems.length - 1 && 'bold'}>{item}</BreadcrumbLink>
+          <BreadcrumbItem
+            key={index}
+            isCurrentPage={index === menuItems.length - 1}
+          >
+            <BreadcrumbLink
+              href={getHref(index)}
+              fontWeight={
+                index === menuItems.length - 1 ? "extrabold" : undefined
+              }
+              color={"gray.600"}
+            >
+              {item}
+            </BreadcrumbLink>
           </BreadcrumbItem>
         );
       })}
@@ -99,12 +94,11 @@ export default function DatasetDirectory({
   directoryContents,
   currentPath,
 }: DatasetDirectoryProps) {
-
-  const nextPath = usePathname()
+  const nextPath = usePathname();
 
   return (
     <Stack spacing={6}>
-      <Box>
+      <Box paddingX={6}>
         <BreadcrumbMenu
           currentPath={currentPath}
           rootPath={dataset.data_store_path}
