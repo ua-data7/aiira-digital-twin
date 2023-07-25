01e582fb5c29b0341ed8cc6d564ad21db4dd6a1a
Michelle Yung <myung@arizona.edu>
2023-07-25 07:51:06 +0000

    start dataset directory breadcrumb menu

--- frontend/src/components/datasets/DatasetDirectory.tsx
+++ frontend/src/components/datasets/DatasetDirectory.tsx
@@ -9,6 +9,14 @@ import {
   TableContainer,
 } from "@chakra-ui/react";
 
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from '@chakra-ui/react'
+
 import { FcFolder, FcFile } from "react-icons/fc";
 import { DownloadIcon } from "@chakra-ui/icons";
 
@@ -20,61 +28,89 @@ type DatasetDirectoryProps = {
   currentPath: string;
 };
 
+function BreadcrumbMenu({currentPath, rootPath}) {
+
+  console.log('current path', currentPath)
+  console.log('root path', rootPath)
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbItem>
+        <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+      </BreadcrumbItem>
+
+      <BreadcrumbItem>
+        <BreadcrumbLink href='#'>Docs</BreadcrumbLink>
+      </BreadcrumbItem>
+
+      <BreadcrumbItem isCurrentPage>
+        <BreadcrumbLink href='#'>Breadcrumb</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
+  )
+}
+
 export default function DatasetDirectory({
   dataset,
   directoryContents,
   currentPath,
 }: DatasetDirectoryProps) {
+
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Size</Th>
-            <Th>Download</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {directoryContents.map((d, index) => {
-            return (
-              <Tr key={index}>
-                <Td>
-                  {d.type === "folder" ? (
-                    <Stack direction="row" spacing={3}>
-                      <Icon as={FcFolder} boxSize={5} />
-                      <Text
+    <Stack>
+      <Box>
+        <BreadcrumbMenu currentPath={currentPath} rootPath={dataset.data_store_path}></BreadcrumbMenu>
+      </Box>
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Size</Th>
+              <Th>Download</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {directoryContents.map((d, index) => {
+              return (
+                <Tr key={index}>
+                  <Td>
+                    {d.type === "folder" ? (
+                      <Stack direction="row" spacing={3}>
+                        <Icon as={FcFolder} boxSize={5} />
+                        <Text
+                          as="a"
+                          href={`/datasets/${dataset.id}/${d.path}`}
+                          color="brand.100"
+                        >
+                          <b>{d.name}</b>
+                        </Text>
+                      </Stack>
+                    ) : (
+                      <Stack direction="row" spacing={3}>
+                        <Icon as={FcFile} boxSize={5} />
+                        <Text>{d.name}</Text>
+                      </Stack>
+                    )}
+                  </Td>
+                  <Td>{d.size ? d.size : "----"}</Td>
+                  <Td>
+                    {d.type === "file" && (
+                      <IconButton
+                        aria-label="Download"
+                        icon={<DownloadIcon />}
                         as="a"
-                        href={`/datasets/${dataset.id}/${d.path}`}
-                        color="brand.100"
-                      >
-                        <b>{d.name}</b>
-                      </Text>
-                    </Stack>
-                  ) : (
-                    <Stack direction="row" spacing={3}>
-                      <Icon as={FcFile} boxSize={5} />
-                      <Text>{d.name}</Text>
-                    </Stack>
-                  )}
-                </Td>
-                <Td>{d.size ? d.size : "----"}</Td>
-                <Td>
-                  {d.type === "file" && (
-                    <IconButton
-                      aria-label="Download"
-                      icon={<DownloadIcon />}
-                      as="a"
-                      href={`https://data.cyverse.org/dav-anon${d.path}`}
-                      download
-                    />
-                  )}
-                </Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-    </TableContainer>
+                        href={`https://data.cyverse.org/dav-anon${d.path}`}
+                        download
+                      />
+                    )}
+                  </Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Stack>
   );
 }
