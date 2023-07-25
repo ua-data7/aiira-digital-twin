263342b5219634bfa9d73129d6a92f1424d99ec0
Michelle Yung <myung@arizona.edu>
2023-07-25 06:58:04 +0000

    limit featured content to apps, software & datasets

--- django/featured/models.py
+++ django/featured/models.py
@@ -5,8 +5,12 @@ from django.contrib.contenttypes.models import ContentType
 class FeaturedContent(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=256, blank=True, null=True)
+    
+    content_type_limit = models.Q(app_label='datasets', model='dataset') \
+        | models.Q(app_label='software', model='software') \
+        | models.Q(app_label='software', model='application')
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, limit_choices_to=content_type_limit, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
 
@@ -14,6 +18,7 @@ class FeaturedContent(models.Model):
         return self.title
 
     class Meta:
+        verbose_name_plural = "featured content"
         indexes = [
             models.Index(fields=["content_type", "object_id"]),
         ]
